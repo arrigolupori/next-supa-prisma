05b0726b9808568f7745b0d565979bf2f44acc4a
Development <dev@airdev.local>
2022-04-13 17:36:00 +0200

    CONFIG: Application with listing POST functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,13 @@
-import '../styles/globals.css';
-import { Toaster } from 'react-hot-toast';
+import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
-  );
+	return (
+		<>
+			<Component {...pageProps} />
+			<Toaster />
+		</>
+	)
 }
 
-export default MyApp;
+export default MyApp
